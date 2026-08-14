ffc5d74cb03fc1c046ae5edaa0aae797392b91c7
Pedro <pepe.ribeiro.lopes@gmail.com>
2026-08-14 09:48:27 -0300

    exemplo04

--- aula02-bigO/big_o_exemplo3.c
+++ aula02-bigO/big_o_exemplo3.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-void imprimirTriangulo(int n)
-{ // n = 5;
-    for (int i = 0; i < n; i++)
-    { //
+void imprimirTriangulo(int n){ // n = 5;
+    for (int i = 0; i < n; i++){ //
         for (int j = 0; j <= i; j++)
         {
             printf("* ");
@@ -15,6 +13,6 @@ void imprimirTriangulo(int n)
 
 int main(void)
 {
-    imprimirTriangulo(5);
+    imprimirTriangulo(5); // somar5; no final entao fica g(n) = 4n**2 + 2n + 10
     return 0;
 }
--- aula02-bigO/big_o_exemplo4.c
+++ aula02-bigO/big_o_exemplo4.c
@@ -2,14 +2,14 @@
 
 void mostrarDobros(int n) {
 
-    for (int i = 1; i < n; i *= 2) {
+    for (int i = 1; i < n; i *= 2) { // o(1); o(n-1); 
         printf("%d\n", i);
     }
 }
 
 int main(void) {
 
-    mostrarDobros(32);
+    mostrarDobros(32); // o(32)
 
     return 0;
 }

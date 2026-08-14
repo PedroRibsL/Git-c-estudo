3236652fa0bc0c660dc7552676e0ce17ea0bd3eb
Pedro <pepe.ribeiro.lopes@gmail.com>
2026-08-14 09:14:29 -0300

    exemplo2
    s

--- aula02-bigO/big_o_exemplo2.c
+++ aula02-bigO/big_o_exemplo2.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-int somaMatriz(int matriz[][3], int n) {
-    int soma = 0;
+int somaMatriz(int matriz[][3], int n)
+{
+    int soma = 0; // o(1)
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            soma += matriz[i][j];
+    for (int i = 0; i < n; i++)
+    { // int i = o(1); n = 3; o(n); o(n)
+        for (int j = 0; j < n; j++)
+        {                         // int j = o(1); n = 3; o(n); o(n)
+            soma += matriz[i][j]; // o(1)
         }
     }
 
-    return soma;
+    return soma; // o(1)
 }
 
-int main(void) {
+int main(void)
+{
     int matriz[3][3] = {
         {1, 2, 3},
         {4, 5, 6},
-        {7, 8, 9}
-    };
+        {7, 8, 9}};
 
-    int resultado = somaMatriz(matriz, 3);
+    int resultado = somaMatriz(matriz, 3); // o(n); 4n**2 + 6n + 4 = 4n**2 + 6n + 5
 
-    printf("Soma = %d\n", resultado);
+    printf("Soma = %d\n", resultado); // o(1)
 
-    return 0;
+    return 0; // o(1)
 }
